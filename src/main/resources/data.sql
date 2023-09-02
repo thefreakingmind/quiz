65a4b8455514887e2253c986dc5fc36bbdedc96f
thefreakingmind <salmansiddiqui100@gmail.com>
2023-09-02 08:48:46 +0530

    Fixed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,109 @@
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	1, 
+	'Which of the following could be written as SIX AND A HALF THOUSAND',
+	'6050',
+	'6500',
+	'6550',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	2, 
+	'A game of football consists of a total playing time of 90 minutes and a break of 15 minutes at half time. A football match begins at 3:25 PM. At what time will the match end',
+	'5:10 PM',
+	'4:15 PM',
+	'4:30 PM',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	3, 
+	'217 + 398 + 783 – 308 + 7 equals',
+	'1195',
+	'1097',
+	'1083',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	4, 
+	'59 – 39 = 29 – ?',
+	'0',
+	'2',
+	'9',
+	3,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	5, 
+	'The number that is 100 less than 10000 is',
+	'Nine thousand',
+	'Nine thousand nine hundred',
+	'Ten thousand nine hundred',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	6, 
+	'Mrs. Gupta has to take a flight at 2 o’clock at night. She goes to bed at twenty minutes past nine and sets the alarm on her clock to wake up in time for her flight. Which of these could be the time for which Mrs. Gupta has set the alarm?',
+	'12:30 AM',
+	'1:00 PM',
+	'11:50 PM',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	7, 
+	'A train leaves town P at 12:05 pm. At what time will it reach town Q, if the journey takes 1 hour and 42 minutes?',
+	'1:37 PM',
+	'1:47 PM',
+	'3:47 PM',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	8, 
+	'Anu says My birthday was 30 days ago but I was away on vacation then. So, I’m throwing a birthday party the day after tomorrow. If Anu’s birthday party is on a Saturday, her birthday was on a',
+	'Tuesday',
+	'Wednesday',
+	'Friday',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	9, 
+	'Which of these numbers is half way between 334 and 364?',
+	'349',
+	'350',
+	'354',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	10, 
+	'309 + 40 + 485 – 49 = 485 + ____',
+	'300',
+	'49',
+	'40',
+	1,
+	-1)
+;
